467e4a6c9ac0a4c29fc7d2ae8f7bc0015f1b6034
Fanchen Meng <fanchem@node0160.palmetto.clemson.edu>
2023-11-04 07:38:31 -0400

    modify metric

--- wo_data/streamlit_app_metric.py
+++ wo_data/streamlit_app_metric.py
@@ -7,6 +7,7 @@ from transformers import BertTokenizer, BertModel
 import torch.nn as nn
 import torch
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -53,23 +54,24 @@ with open("../answers.txt", 'r') as f:
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
-class BertEmbedding(nn.Module):
-    def __init__(self, dropout=0.5):
-        #super(BertClassifier, self).__init__()
-        super(BertEmbedding, self).__init__()
-        self.bert = BertModel.from_pretrained('bert-base-cased',output_hidden_states=True)
-    def forward(self, input_id, mask):
-        _, pooled_output,_hidden_states = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
-        # Getting embeddings from the final BERT layer
-        token_embeddings = _hidden_states[-1]
-        # Collapsing the tensor into 1-dimension
-        token_embeddings = torch.squeeze(token_embeddings, dim=0)
-        # Converting torchtensors to lists
-        list_token_embeddings = [token_embed.tolist() for token_embed in token_embeddings]
-        return token_embeddings
+# class BertEmbedding(nn.Module):
+#     def __init__(self, dropout=0.5):
+#         #super(BertClassifier, self).__init__()
+#         super(BertEmbedding, self).__init__()
+#         self.bert = BertModel.from_pretrained('bert-base-cased',output_hidden_states=True)
+#     def forward(self, input_id, mask):
+#         _, pooled_output,_hidden_states = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+#         # Getting embeddings from the final BERT layer
+#         token_embeddings = _hidden_states[-1]
+#         # Collapsing the tensor into 1-dimension
+#         token_embeddings = torch.squeeze(token_embeddings, dim=0)
+#         # Converting torchtensors to lists
+#         list_token_embeddings = [token_embed.tolist() for token_embed in token_embeddings]
+#         return token_embeddings
+# model = BertEmbedding(dropout=0)
 
-model = BertEmbedding(dropout=0)
-
+model = BertModel.from_pretrained('bert-base-cased')
+cossim_total = []
 for idx, question in enumerate(questions):
     prompt = question
 #if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
@@ -89,19 +91,30 @@ for idx, question in enumerate(questions):
             st.write(response.response)
             res_token = tokenizer(
                 [response.response], max_length=256, padding='max_length', truncation=True)
-            res = model(
-                torch.tensor(res_token['input_ids']), torch.tensor(res_token['attention_mask']))
+            with torch.no_grad():
+                res = model(
+                    torch.tensor(res_token['input_ids']), torch.tensor(res_token['attention_mask']))
+            res_hidden = res.last_hidden_state[:,0,:]
             st.write("Correct answer: "+ answers[idx])
             std_token = tokenizer(
                 [answers[idx]], max_length=256, padding='max_length', truncation=True)
-            std = model(
-                torch.tensor(std_token['input_ids']), torch.tensor(std_token['attention_mask']))
-            cossim = cosine_similarity(
-                res.view(1,-1).detach().numpy(), std.view(1,-1).detach().numpy())
+            with torch.no_grad():
+                std = model(
+                    torch.tensor(std_token['input_ids']), torch.tensor(std_token['attention_mask']))
+            std_hidden = std.last_hidden_state[:,0,:]
+            cossim = cosine_similarity(res_hidden.numpy(), std_hidden.numpy())
+            cossim_total.append(cossim[0][0])
             st.write("Cosine Similarity = " + str(cossim[0][0]))
+
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
 
+ave = np.mean(cossim_total)
+if st.session_state.messages[-1]["role"] == "assistant":           
+    with st.chat_message("assistant"):
+        with st.spinner("Calculating averate cosine similarity..."):
+            st.write("Average Cosine Similarity = "+str(ave))
+                     
 #if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
 #    st.session_state.messages.append({"role": "user", "content": prompt})
 
