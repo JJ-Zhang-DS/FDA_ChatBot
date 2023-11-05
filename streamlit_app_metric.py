a737f5ddc981b8698bdc1e99c0ebaa8d8d525a0d
Joel <JJ.zhang.in@gmail.com>
2023-11-05 11:02:40 -0500

    Add files via upload

--- streamlit_app_metric.py
+++ streamlit_app_metric.py
@@ -19,40 +19,34 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
 
-@st.cache_resource(show_spinner=False)
+#@st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+        st.session_state.chat_engine = index.as_query_engine() #chat_mode="condense_question", verbose=True)
 
 # here will be the test for example questions
 
 questions = []
 answers = []
-with open("./questions.txt", 'r') as f:
+with open("../questions.txt", 'r') as f:
     for line in f.readlines():
-        print(line)
         questions.append(line.split('\n')[0])
-with open("./answers.txt", 'r') as f:
+with open("../answers.txt", 'r') as f:
     for line in f.readlines():
-        print(line)
         answers.append(line.split('\n')[0])
 
-#chat_answers = []
-#for question in questions:
-#    promt = question
-#    responses = st.session_state.chat_engine.chat(prompt)
-#    chat_answers.append(responses)
+assert len(questions) == len(answers)
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
 
 # class BertEmbedding(nn.Module):
 #     def __init__(self, dropout=0.5):
@@ -70,24 +64,21 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 #         return token_embeddings
 # model = BertEmbedding(dropout=0)
 
-model = BertModel.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-large-uncased')
 cossim_total = []
+sorry_words = ["sorry", "not mention", "not provide", "no information", "no data"]
+
 for idx, question in enumerate(questions):
     prompt = question
-#if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    
-    with st.chat_message(st.session_state.messages[-1]["role"]):
-        st.write(st.session_state.messages[-1]["content"])
-#for message in st.session_state.messages: # Display the prior chat messages
-#    with st.chat_message(message["role"]):
-#        st.write(message["content"])
-
-# If last message is not from assistant, generate a new response
-#if st.session_state.messages[-1]["role"] != "assistant":
+ #   st.session_state.messages.append({"role": "user", "content": prompt})
+ #   
+ #   with st.chat_message(st.session_state.messages[-1]["role"]):
+    with st.chat_message("user"):
+ #       st.write(st.session_state.messages[-1]["content"])
+        st.write(prompt)
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
+            response = st.session_state.chat_engine.query(prompt)
             st.write(response.response)
             res_token = tokenizer(
                 [response.response], max_length=128, padding='max_length', truncation=True)
@@ -102,12 +93,19 @@ for idx, question in enumerate(questions):
                 std = model(
                     torch.tensor(std_token['input_ids']), torch.tensor(std_token['attention_mask']))
             std_hidden = std.last_hidden_state[:,0,:]
-            cossim = cosine_similarity(res_hidden.numpy(), std_hidden.numpy())
+            cossim = [[1]]
+#            for sorry in sorry_words:
+#                if sorry in response.response:
+#                    cossim = [[0]]
+#                    break
+            
+            if cossim[0][0] == 1:
+                cossim = cosine_similarity(res_hidden.numpy(), std_hidden.numpy())
             cossim_total.append(cossim[0][0])
             st.write("Cosine Similarity = " + str(cossim[0][0]))
 
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+#            message = {"role": "assistant", "content": response.response}
+#            st.session_state.messages.append(message) # Add response to message history
 
 ave = np.mean(cossim_total)
 if st.session_state.messages[-1]["role"] == "assistant":           
@@ -115,18 +113,3 @@ if st.session_state.messages[-1]["role"] == "assistant":
         with st.spinner("Calculating averate cosine similarity..."):
             st.write("Average Cosine Similarity = "+str(ave))
                      
-#if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-#    st.session_state.messages.append({"role": "user", "content": prompt})
-
-#for message in st.session_state.messages: # Display the prior chat messages
-#    with st.chat_message(message["role"]):
-#        st.write(message["content"])
-
-# If last message is not from assistant, generate a new response
-#if st.session_state.messages[-1]["role"] != "assistant":
-#    with st.chat_message("assistant"):
-#        with st.spinner("Thinking..."):
-#            response = st.session_state.chat_engine.chat(prompt)
-#            st.write(response.response)
-#            message = {"role": "assistant", "content": response.response}
-#            st.session_state.messages.append(message) # Add response to message history
