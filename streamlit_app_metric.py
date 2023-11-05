44174fde3bd9d36d0344349ed0a2fb457e1cf236
Joel <JJ.zhang.in@gmail.com>
2023-11-05 14:28:47 -0500

    Update streamlit_app_metric.py

--- streamlit_app_metric.py
+++ streamlit_app_metric.py
@@ -9,9 +9,9 @@ import torch
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with the FDA docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
+st.title("Chat with the FDA docs")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
