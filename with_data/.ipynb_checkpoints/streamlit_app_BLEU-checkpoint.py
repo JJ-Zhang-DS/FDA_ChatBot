5920d376847c0cbb581fd69890221d6cf4cf64df
Fanchen Meng <fanchem@node1271.palmetto.clemson.edu>
2023-11-05 00:09:01 -0400

    update

--- with_data/.ipynb_checkpoints/streamlit_app_BLEU-checkpoint.py
+++ with_data/.ipynb_checkpoints/streamlit_app_BLEU-checkpoint.py
@@ -13,9 +13,7 @@ st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaInd
 openai.api_key = st.secrets.openai_key
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
-
-st.cache_data(func=None, *, max_entries=0)
-
+         
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
@@ -26,7 +24,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
@@ -54,7 +52,7 @@ with open("../answers.txt", 'r') as f:
 #    responses = st.session_state.chat_engine.chat(prompt)
 #    chat_answers.append(responses)
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+#tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # class BertEmbedding(nn.Module):
 #     def __init__(self, dropout=0.5):
@@ -72,8 +70,12 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 #         return token_embeddings
 # model = BertEmbedding(dropout=0)
 
-model = BertModel.from_pretrained('bert-base-uncased')
-cossim_total = []
+#model = BertModel.from_pretrained('bert-base-uncased')
+
+from nltk.translate.bleu_score import sentence_bleu
+nltk.download('punkt')
+
+sim_total = []
 for idx, question in enumerate(questions):
     prompt = question
 #if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
@@ -91,31 +93,39 @@ for idx, question in enumerate(questions):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
-            res_token = tokenizer(
-                [response.response], max_length=128, padding='max_length', truncation=True)
-            with torch.no_grad():
-                res = model(
-                    torch.tensor(res_token['input_ids']), torch.tensor(res_token['attention_mask']))
-            res_hidden = res.last_hidden_state[:,0,:]
-            st.write("Correct answer: "+ answers[idx])
-            std_token = tokenizer(
-                [answers[idx]], max_length=128, padding='max_length', truncation=True)
-            with torch.no_grad():
-                std = model(
-                    torch.tensor(std_token['input_ids']), torch.tensor(std_token['attention_mask']))
-            std_hidden = std.last_hidden_state[:,0,:]
-            cossim = cosine_similarity(res_hidden.numpy(), std_hidden.numpy())
-            cossim_total.append(cossim[0][0])
-            st.write("Cosine Similarity = " + str(cossim[0][0]))
+            # res_token = tokenizer(
+            #     [response.response], max_length=128, padding='max_length', truncation=True)
+            # with torch.no_grad():
+            #     res = model(
+            #         torch.tensor(res_token['input_ids']), torch.tensor(res_token['attention_mask']))
+            # res_hidden = res.last_hidden_state[:,0,:]
 
+            res = [response.response]
+            
+            st.write("Correct answer: "+ answers[idx])
+            std = answers[idx]
+            bleu = sentence_bleu(res, std)
+            # std_token = tokenizer(
+            #     [answers[idx]], max_length=128, padding='max_length', truncation=True)
+            # with torch.no_grad():
+            #     std = model(
+            #         torch.tensor(std_token['input_ids']), torch.tensor(std_token['attention_mask']))
+            # std_hidden = std.last_hidden_state[:,0,:]
+            
+            # cossim = cosine_similarity(res_hidden.numpy(), std_hidden.numpy())
+            # sim_total.append(cossim[0][0])
+
+            sim_total.append(bleu)
+            # st.write("Cosine Similarity = " + str(cossim[0][0]))
+            st.write("BLEU score = " + str(bleu))
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
 
-ave = np.mean(cossim_total)
+ave = np.mean(sim_total)
 if st.session_state.messages[-1]["role"] == "assistant":           
     with st.chat_message("assistant"):
-        with st.spinner("Calculating averate cosine similarity..."):
-            st.write("Average Cosine Similarity = "+str(ave))
+        with st.spinner("Calculating average similarity..."):
+            st.write("Average Similarity = "+str(ave))
                      
 #if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
 #    st.session_state.messages.append({"role": "user", "content": prompt})
